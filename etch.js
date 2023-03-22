b78d64a3305785226a5645401fc755a3bae24d82
hupalex <alex.hupuczi@gmail.com>
2023-03-22 13:44:30 +0100

    Implement coloring when hover through divs, and a start button

--- etch.js
+++ etch.js
@@ -1,16 +1,30 @@
-function initGrid(){
-    for(i = 1; i <= 256; i++){
-        const gridItem = document.createElement("div");
-        const gridContainer = document.querySelector("#grid-container");
-        
-        gridItem.classList.add("grid-item");
-        
-        gridContainer.appendChild(gridItem);
-    }
+//loop 256 times, create divs, adding them grid-item class and add them into the grid-container
+function initGrid(numCells){
+    const startBtn = document.querySelector("#start");
+    startBtn.addEventListener("click", () => {
+        numCells = prompt("What should the width of the table be?");
+        for(i = 1; i <= numCells; i++){
+            const initGridItem = document.createElement("div");
+            const gridContainer = document.querySelector("#grid-container");
+                
+            initGridItem.classList.add("grid-item");
+            gridContainer.appendChild(initGridItem);
+        }
+    })
+}
+initGrid();
+
+const allGridItem = document.querySelectorAll("div");
+    
+allGridItem.forEach((grid) => {
+    grid.addEventListener("mouseover", e => {
+        e.target.style.backgroundColor = "grey";
+    })
+})
+
 
     
 
-        
 
-}
-initGrid();
+
+
