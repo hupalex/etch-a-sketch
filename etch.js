280266e681514ccb76e0bcbb19b423e550725070
hupalex <alex.hupuczi@gmail.com>
2023-03-22 15:19:08 +0100

    Pressing the button again makes the grid clear and ask for prompt from user again.

--- etch.js
+++ etch.js
@@ -1,26 +1,52 @@
+let itemsToRemove = document.getElementsByClassName("grid-item");
+
 //loop 256 times, create divs, adding them grid-item class and add them into the grid-container
 function initGrid(numCells){
     const startBtn = document.querySelector("#start");
     startBtn.addEventListener("click", () => {
-        numCells = prompt("What should the width of the table be?");
-        for(i = 1; i <= numCells; i++){
-            const initGridItem = document.createElement("div");
-            const gridContainer = document.querySelector("#grid-container");
-                
-            initGridItem.classList.add("grid-item");
-            gridContainer.appendChild(initGridItem);
-        }
+        console.log(itemsToRemove);
+
+        if(itemsToRemove.length === 0){
+            numCells = prompt("What should the width of the table be?");
+ 
+            for(i = 0; i <= numCells * numCells; i++){
+                const initGridItem = document.createElement("div");
+                const gridContainer = document.querySelector("#grid-container");
+                    
+                initGridItem.classList.add("grid-item");
+                gridContainer.appendChild(initGridItem);
+            }
+        }else {
+            removeGrids();
+            numCells = prompt("What should the width of the table be?");
+            for(i = 0; i <= numCells*numCells; i++){
+                const initGridItem = document.createElement("div");
+                const gridContainer = document.querySelector("#grid-container");
+                    
+                initGridItem.classList.add("grid-item");
+                gridContainer.appendChild(initGridItem);
+            }
+        }       
     })
+    
 }
 initGrid();
 
+function removeGrids(){ 
+    let item;
+    while(item = itemsToRemove[0]){
+        item.parentNode.removeChild(item);
+        
+    }    
+}
+
 const allGridItem = document.querySelectorAll("div");
-    
+
 allGridItem.forEach((grid) => {
     grid.addEventListener("mouseover", e => {
         e.target.style.backgroundColor = "grey";
     })
-})
+});
 
 
     
